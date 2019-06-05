2d884ce12ff8e03b9dda30477d72c99e7995a3a9
ShreyaRawal97 <50499340+ShreyaRawal97@users.noreply.github.com>
2019-06-05 08:32:23 -0500

    Add files via upload

--- Task4.py
+++ Task4.py
@@ -25,24 +25,19 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
-set1 = set()
-set2 = set()
-set3 = set()
-set4 = set()
-
-for i in calls:
-    if i[0][0] == "7" or i[0][0] == "8" or i[0][0] == "9" or i[0][0] == "(" or i[0][:3] == "140":
-        set1.add(i[0])
-    if i[1][0] == "7" or i[1][0] == "8" or i[1][0] == "9" or i[1][0] == "(" or i[1][:3] == "140":
-        set2.add(i[1])
-
+set1 = set(i[0] for i in calls)
+set2 = set(i[1] for i in calls)
 set3 = set1.difference(set2)
-
-for i in texts:
-    set4.add(i[0])
-    set4.add(i[1])
+set4 = set(i[0] for i in texts)
+set4 = set(i[1] for i in texts)
 
 telemarketers_set = set()
 telemarketers_set = set3.difference(set4)
 print("These numbers could be telemarketers:")
 print("\n".join(sorted(telemarketers_set)))
+
+
+#Big-O Analysis
+"""
+O(cardinality of telemarketers_set)
+"""
